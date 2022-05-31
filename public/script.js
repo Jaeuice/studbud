4d96951c078de5fa2434566e73cb19f4f03b7ebf
Jaeuice <100391108+Jaeuice@users.noreply.github.com>
2022-05-31 22:18:00 +1000

    Create a basic nav bar

---
+++ public/script.js
@@ -0,0 +1,12 @@
+// Set the element need to return which is used as selecting button
+let items=document.querySelectorAll('.item');
+
+function buttonClick(){
+    items.forEach((item)=>{
+        item.classList.remove('active');
+    })
+    this.classList.add('active');
+}
+// Set clicking action for each item
+items.forEach((item)=>{
+    item.addEventListener('click',buttonClick)})
